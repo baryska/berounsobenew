eb8e6e2c2817dd7350fc1e3f7491007d75309523
baryska <barbora.skalova@newtontech.cz>
2022-11-26 22:47:51 +0100

    make posts page responsive

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -11,14 +11,19 @@ const menuItems = [
   { title: 'Urbanismus', key: 3 }
 ]
 
-
-
 const News = () => {
-  const [posts, setPosts] = useState(Posts)
+  const [posts, setPosts] = useState(Posts);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const handleFilterPosts = (key: number) => {
     const filteredPosts = Posts.filter((post) => post.key === key)
     setPosts(filteredPosts)
+    setDropdownOpen(false)
+  }
+
+  const handleChooseAllPosts = () => {
+    setPosts(Posts);
+    setDropdownOpen(false);
   }
 
   return (
@@ -27,10 +32,27 @@ const News = () => {
         <div className={styles.allPosts__menu}>
           <Link href="/#informujeme"><a className={styles.arrowBack}><Image src="/arrow4.svg" alt="sipka" width="40px" height="40px" /></a></Link>
           <div className={styles.menu}>
-            {menuItems.map((item) =>
-              <h1 key={item.title} className={styles.menuItem} onClick={() => handleFilterPosts(item.key)}>{item.title}</h1>
-            )}
-            <h1 className={styles.menuItem} style={{ fontStyle: 'italic', marginTop: '2rem' }} onClick={() => setPosts(Posts)}>Vše</h1>
+            <div className={styles.menuSmall}>
+              <button className={styles.dropdownButton} onClick={() => setDropdownOpen(!dropdownOpen)}>Témata<svg className="ml-2 w-4 h-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg></button>
+              {dropdownOpen && (
+                <div className=" z-10 w-52 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
+                  <ul className="py-1 text-sm text-gray-700 dark:text-gray-200">
+                    {menuItems.map((item) =>
+                      <li key={item.key} style={{ margin: '0.5rem auto' }} onClick={() => handleFilterPosts(item.key)}>
+                        <a href="#" className={styles.menuItem}>{item.title}</a>
+                      </li>
+                    )}
+                    <li className={styles.menuItem} style={{ fontStyle: 'italic', marginTop: '2rem' }} onClick={() => handleChooseAllPosts()}>Vše</li>
+                  </ul>
+                </div>
+              )}
+            </div>
+            <div className={styles.menuLarge}>
+              {menuItems.map((item) =>
+                <h1 key={item.title} className={styles.menuItem} onClick={() => handleFilterPosts(item.key)}>{item.title}</h1>
+              )}
+              <h1 className={styles.menuItem} style={{ fontStyle: 'italic', marginTop: '2rem' }} onClick={() => setPosts(Posts)}>Vše</h1>
+            </div>
           </div>
         </div>
 
