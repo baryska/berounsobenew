4254bac0186be01b7556094417bd93643cbe0b62
baryska <barbora.skalova@newtontech.cz>
2023-07-16 21:39:53 +0200

    add programme points

--- pages/program.tsx
+++ pages/program.tsx
@@ -8,6 +8,18 @@ const ProgrammePage = () => {
   const router = useRouter();
   return (
     <>
+     <div className={styles.programme__info}>Strategický plán investic & transparentnost:</div>
+      <div className={styles.programme__download}><a
+        href="/plan_investic.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+      >Stáhnout v PDF</a></div>
+       <div className={styles.programme__info}>Koncepce školství:</div>
+      <div className={styles.programme__download}><a
+        href="/koncepce_skolstvi.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+      >Stáhnout v PDF</a></div>
       <div className={styles.programme__info}>Program, se kterým jsme šli do komunálních voleb 2022:</div>
       <div className={styles.programme__download}><a
         href="/program_Berounsobe.pdf"
