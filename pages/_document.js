b29684a3127f2690a7c9cfb595ca2dd846a2457c
baryska <baryska@gmail.com>
2026-06-12 23:39:14 +0200

    fix some errors

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" href="/nove_logo.png" type="image/png" sizes="any" />
+          <link rel="icon" href="/nove_logo.png?v=2" type="image/png" sizes="any" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link href="https://fonts.googleapis.com/css2?family=Courier+Prime&family=Cutive&family=Lekton&family=Oswald:wght@200;300;400;500;600;700&family=Inria+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&family=Special+Elite&display=swap" rel="stylesheet"/>
